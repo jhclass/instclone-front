954a58be6a01b271f150c8e516580b906471cc6c
Jinhyoung Lee <jhcode0@naver.com>
2023-06-13 19:07:57 +0900

    deleteComment (cache.evict())

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,6 +1,16 @@
 import { PropTypes } from "prop-types";
 import styled from "styled-components";
 import { FeedPadding, FatText, SmallText } from "../shared";
+import { gql } from "apollo-client-preset";
+import { useMutation } from "@apollo/client";
+
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComent($id: Int!) {
+    deleteComment(id: $id) {
+      ok
+    }
+  }
+`;
 
 const FeedComments = styled(FeedPadding)`
   color: ${(props) => props.theme.fontColor};
@@ -18,7 +28,28 @@ const changedTime = (createdAt) => {
 
   return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초 작성됨.`;
 };
-const Comment = ({ user, payload, createdAt }) => {
+const Comment = ({ user, payload, createdAt, id, isMine }) => {
+  //console.log(isMine);
+  const updateMutation = (cache, result) => {
+    const {
+      data: {
+        deleteComment: { ok },
+      },
+    } = result;
+    if (ok) {
+      console.log(id);
+      cache.evict({ id: `Comment:${id}` });
+    }
+  };
+  const [deleteComentMutation] = useMutation(DELETE_COMMENT_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateMutation,
+  });
+  const onDeleteClick = () => {
+    deleteComentMutation();
+  };
   return (
     <FeedComments>
       <div>
@@ -30,6 +61,7 @@ const Comment = ({ user, payload, createdAt }) => {
         <SmallText style={{ marginTop: "5px", display: "inline-block" }}>
           {changedTime(Number(createdAt))}
         </SmallText>
+        {isMine ? <button onClick={onDeleteClick}>삭제</button> : null}
       </div>
       <div></div> {/** 대댓글 */}
     </FeedComments>
@@ -43,6 +75,8 @@ Comment.propTypes = {
   }),
   payload: PropTypes.string,
   createdAt: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  isMine: PropTypes.bool.isRequired,
 };
 
 export default Comment;
