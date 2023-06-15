ca1136234544f89a5f2dc2247d72ddab85032400
Jinhyoung Lee <jhcode0@naver.com>
2023-06-15 19:27:16 +0900

    seeProfile -2 (useQuery)

--- src/screen/Profile.js
+++ src/screen/Profile.js
@@ -1,6 +1,7 @@
 import { gql } from "apollo-client-preset";
 import { useParams } from "react-router-dom";
 import { PHOTO_FRAGMENT } from "./fragment";
+import { useQuery } from "@apollo/client";
 
 const SEE_PROFILE_QUERY = gql`
   query seeProfile($username: String!) {
@@ -25,7 +26,13 @@ const SEE_PROFILE_QUERY = gql`
 
 const Profile = () => {
   const { username } = useParams();
-  console.log(username);
+  const { data } = useQuery(SEE_PROFILE_QUERY, {
+    variables: {
+      username,
+    },
+  });
+
+  console.log(data);
   return <>asdfasdf</>;
 };
 export default Profile;
